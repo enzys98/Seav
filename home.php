a5c7360046574f54b1341e94fe3d032a3ee06334
enzys98 <mariovincenzo.guastafierro001@studenti.uniparthenope.it>
2022-07-21 15:01:19 +0200

    updt

--- home.php
+++ home.php
@@ -83,7 +83,7 @@
         
             <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
                 <div class="col-xl-6 col-lg-8">
-                <img src="assets/img/logo.png" alt="logo seav" class="img-fluid" style="background: rgba(0, 0, 0, 0.6);">
+                <img src="assets/img/logo.png" alt="logo seav" class="img-fluid">
                     <!--
                     <h1>Le migliori soluzioni per gli impianti elettrici<span>.</span></h1>
                     <h2>Frase da inserire</h2> -->
