5e5a0c6d778d215109f79990fbcb4bb2b4c844d8
enzys98 <mariovincenzo.guastafierro001@studenti.uniparthenope.it>
2022-10-31 17:21:16 +0100

    nuove modofiche

--- home.php
+++ home.php
@@ -1,13 +1,8 @@
 <?php
 $dir = "/assets/img/automazione";
 
-// Open a directory, and read its contents
-if (is_dir($dir)){
-  if ($dh = opendir($dir)){
-    while (($file = readdir($dh)) !== false){
-      echo "filename:" . $file . "<br>";
-    }
-    closedir($dh);
-  }
-}
+$a = scandir($dir);
+
+print_r($a);
+?>
 
