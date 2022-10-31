c2ef8867bcc6069b0f79f3d3d15fd6f110751e16
enzys98 <mariovincenzo.guastafierro001@studenti.uniparthenope.it>
2022-10-31 18:05:49 +0100

    nuove modofiche

--- utilityAutomazione.js
+++ utilityAutomazione.js
@@ -1,9 +1,11 @@
-fetch("/home.php").then(async response => {
-    try {
-        const data = await response.json()
-        console.log('response data?', data)
-    } catch(error) {
-        console.log('Error happened here!')
-        console.error(error)
+let ajax = new XMLHttpRequest();
+let method = "GET";
+let url = "home.php";
+ajax.open(method,url,true);
+ajax.send();
+
+ajax.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        alert(this.responseText);
     }
-})
+}
