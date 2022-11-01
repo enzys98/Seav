530421c4fe723174e09f8436588a63109892d0d1
enzys98 <mariovincenzo.guastafierro001@studenti.uniparthenope.it>
2022-11-01 17:55:29 +0100

    nuove modofiche

--- assets/js/utilityPBlock.js
+++ assets/js/utilityPBlock.js
@@ -6,6 +6,23 @@ ajax.send();
 
 ajax.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
+        //console.log(this.responseText);
+        this.responseText.forEach(element => {
+            console.log('element --> ' , element);
+            /*
+            if(element.includes('.jpg') || element.includes('.jpeg') || element.includes('.png') || element.includes('.JPG')) {
+                let divImage = document.querySelector('#gallery');
+                let divCol = document.createElement('div');
+                divCol.classList.add("col-sm-6");
+                divCol.classList.add("col-md-3");
+                divCol.classList.add("mb-3");
+                let img = document.createElement('img');
+                img.src = "../assets/img/distribuzione/" +element;
+                divCol.appendChild(img);
+                divImage.appendChild(divCol);
+            }
+
+             */
+        });
     }
 }
