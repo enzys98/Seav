f7195c89b089393a7a64342e005a94f1a28fa4b2
enzys98 <mariovincenzo.guastafierro001@studenti.uniparthenope.it>
2022-11-01 19:27:37 +0100

    nuove modofiche

--- assets/js/utilityPBlock.js
+++ assets/js/utilityPBlock.js
@@ -10,18 +10,17 @@ ajax.onreadystatechange = function () {
         let data = JSON.parse(this.responseText);
         //console.log(data);
         data.forEach(element => {
-            console.log('element --> ' , element);
-
             if(element.includes('.jpg') || element.includes('.jpeg') || element.includes('.png') || element.includes('.JPG')) {
-                let divImage = document.querySelector('#gallery');
-                let divCol = document.createElement('div');
-                divCol.classList.add("col-sm-6");
-                divCol.classList.add("col-md-3");
-                divCol.classList.add("mb-3");
-                let img = document.createElement('img');
-                img.src = "../assets/img/distribuzione/" +element;
-                divCol.appendChild(img);
-                divImage.appendChild(divCol);
+                console.log('element --> ' , element);
+                // let divImage = document.querySelector('#gallery');
+                // let divCol = document.createElement('div');
+                // divCol.classList.add("col-sm-6");
+                // divCol.classList.add("col-md-3");
+                // divCol.classList.add("mb-3");
+                // let img = document.createElement('img');
+                // img.src = "../assets/img/distribuzione/" +element;
+                // divCol.appendChild(img);
+                // divImage.appendChild(divCol);
             }
 
 
