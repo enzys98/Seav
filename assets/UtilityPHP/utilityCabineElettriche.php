22a0c7d8eef577347ca00133e813a4ba67a153c2
enzys98 <mariovincenzo.guastafierro001@studenti.uniparthenope.it>
2022-11-02 21:11:42 +0100

    nuove modofiche

---
+++ assets/UtilityPHP/utilityCabineElettriche.php
@@ -0,0 +1,13 @@
+<?php
+$dir = "../img/impiantiElettrici/";
+$impiantiElettriciArray = array();
+// Open a directory, and read its contents
+if (is_dir($dir)){
+    if ($dh = opendir($dir)){
+        while (($file = readdir($dh)) !== false){
+            $impiantiElettriciArray[] = $file;
+        }
+        closedir($dh);
+    }
+}
+echo json_encode($impiantiElettriciArray);
