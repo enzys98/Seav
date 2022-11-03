601224ade8049ef4bf057f7024c1babba9092e79
enzys98 <mariovincenzo.guastafierro001@studenti.uniparthenope.it>
2022-11-03 16:00:47 +0100

    creazione carousel

--- home.php
+++ home.php
@@ -360,19 +360,19 @@
 
                 <div class="mySlides fade">
                     <div class="numbertext">1 / 3</div>
-                    <img src="assets/img/storia.jpeg" style="width:100%">
+                    <img src="assets/img/storia.jpeg" class="img-fluid">
                     <div class="text">Caption Text</div>
                 </div>
 
                 <div class="mySlides fade">
                     <div class="numbertext">2 / 3</div>
-                    <img src="assets/img/storia.jpeg" style="width:100%">
+                    <img src="assets/img/storia.jpeg" class="img-fluid">
                     <div class="text">Caption Two</div>
                 </div>
 
                 <div class="mySlides fade">
                     <div class="numbertext">3 / 3</div>
-                    <img src="assets/img/storia.jpeg" style="width:100%">
+                    <img src="assets/img/storia.jpeg" class="img-fluid">
                     <div class="text">Caption Three</div>
                 </div>
 
