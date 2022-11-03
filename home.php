bf405c4280058c033b5a16522e0b98e44a31ee5e
enzys98 <mariovincenzo.guastafierro001@studenti.uniparthenope.it>
2022-11-03 15:56:46 +0100

    creazione carousel

--- home.php
+++ home.php
@@ -355,35 +355,36 @@
             </div>
         </section> -->
         <!-- End Cta Section -->
+        <section id="carousel" style="height: 400px" >
+            <div class="slideshow-container" style="height: 400px">
 
-        <div class="slideshow-container" style="height: 400px">
+                <div class="mySlides fade">
+                    <div class="numbertext">1 / 3</div>
+                    <img src="assets/img/storia.jpeg" style="width:100%">
+                    <div class="text">Caption Text</div>
+                </div>
 
-            <div class="mySlides fade">
-                <div class="numbertext">1 / 3</div>
-                <img src="assets/img/storia.jpeg" style="width:100%">
-                <div class="text">Caption Text</div>
+                <div class="mySlides fade">
+                    <div class="numbertext">2 / 3</div>
+                    <img src="assets/img/storia.jpeg" style="width:100%">
+                    <div class="text">Caption Two</div>
+                </div>
+
+                <div class="mySlides fade">
+                    <div class="numbertext">3 / 3</div>
+                    <img src="assets/img/storia.jpeg" style="width:100%">
+                    <div class="text">Caption Three</div>
+                </div>
+
             </div>
+            <br>
 
-            <div class="mySlides fade">
-                <div class="numbertext">2 / 3</div>
-                <img src="assets/img/storia.jpeg" style="width:100%">
-                <div class="text">Caption Two</div>
+            <div style="text-align:center">
+                <span class="dot"></span>
+                <span class="dot"></span>
+                <span class="dot"></span>
             </div>
-
-            <div class="mySlides fade">
-                <div class="numbertext">3 / 3</div>
-                <img src="assets/img/storia.jpeg" style="width:100%">
-                <div class="text">Caption Three</div>
-            </div>
-
-        </div>
-        <br>
-
-        <div style="text-align:center">
-            <span class="dot"></span>
-            <span class="dot"></span>
-            <span class="dot"></span>
-        </div>
+        </section>
 
 
         <!-- ======= Counts Section ======= -->
