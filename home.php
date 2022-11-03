9c32621fbf7acb741ddccb2fdcf117f740e7b62b
enzys98 <mariovincenzo.guastafierro001@studenti.uniparthenope.it>
2022-11-03 15:54:11 +0100

    creazione carousel

--- home.php
+++ home.php
@@ -356,7 +356,7 @@
         </section> -->
         <!-- End Cta Section -->
 
-        <div class="slideshow-container">
+        <div class="slideshow-container" style="height: 400px">
 
             <div class="mySlides fade">
                 <div class="numbertext">1 / 3</div>
